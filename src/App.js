abc8bae0fe213e2f8f7719b1b87f16c899161917
Nikita Zotcik <nikita.exceed@gmail.com>
2019-06-17 17:07:41 +0300

    Added chat

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 class App extends Component {
   componentDidMount() {
     axios("https://crypto-project-backend.herokuapp.com/api/getCoinsFromDb")
+    // axios("http://localhost:5000/api/getCoinsFromDb")
       .then(result => {
         return result.data.coins;
       })
@@ -71,25 +72,81 @@ class App extends Component {
           headerName: "Website",
           field: "website",
           sortable: true,
-          filter: true
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.website) {
+              let link = document.createElement("a");
+              link.href = params.data.website;
+              link.innerText = params.data.website;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
         },
         {
           headerName: "Twitter",
           field: "twitter",
           sortable: true,
-          filter: true
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.twitter) {
+              let link = document.createElement("a");
+              link.href = params.data.twitter;
+              link.innerText = params.data.twitter;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
+        },
+        {
+          headerName: "Chat",
+          field: "chat",
+          sortable: true,
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.chat) {
+              let link = document.createElement("a");
+              link.href = params.data.chat;
+              link.innerText = params.data.chat;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
         },
         {
           headerName: "Reddit",
           field: "reddit",
           sortable: true,
-          filter: true
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.reddit) {
+              let link = document.createElement("a");
+              link.href = params.data.reddit;
+              link.innerText = params.data.reddit;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
         },
         {
           headerName: "Message Board",
           field: "message_board",
           sortable: true,
-          filter: true
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.message_board) {
+              let link = document.createElement("a");
+              link.href = params.data.message_board;
+              link.innerText = params.data.message_board;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
         },
         {
           headerName: "Explorer",
@@ -101,7 +158,17 @@ class App extends Component {
           headerName: "Technical Doc",
           field: "technical_doc",
           sortable: true,
-          filter: true
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.technical_doc) {
+              let link = document.createElement("a");
+              link.href = params.data.technical_doc;
+              link.innerText = params.data.technical_doc;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
         },
         {
           headerName: "Tags",
@@ -113,7 +180,17 @@ class App extends Component {
           headerName: "Source Code",
           field: "source_code",
           sortable: true,
-          filter: true
+          filter: true,
+          cellRenderer: params => {
+            if (params.data.source_code) {
+              let link = document.createElement("a");
+              link.href = params.data.source_code;
+              link.innerText = params.data.source_code;
+              link.target = "_blank";
+
+              return link;
+            }
+          }
         },
         {
           headerName: "Date Added",
