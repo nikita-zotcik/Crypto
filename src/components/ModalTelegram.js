fb1a8cccf050ff4576cf9c8ac738e9df7b9e7c64
Evgeniy <egromov.exceedteam@gmail.com>
2019-07-01 15:58:20 +0300

    telegram api fix

--- src/components/ModalTelegram.js
+++ src/components/ModalTelegram.js
@@ -20,7 +20,7 @@ class ModalTelegram extends Component {
     axiosRequest = (link) => {
         console.debug(link);
 
-        axios(`http://localhost:5000/api/getTelegram?link=${link}`)
+        axios(`https://crypto-project-frontend.herokuapp.com/:5000/api/getTelegram?link=${link}`)
             .then(result => {
                 if (result.data === "CODE#400 CHANNEL_INVALID") {
                     this.setState({
